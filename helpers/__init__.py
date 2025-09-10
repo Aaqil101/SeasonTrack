aa73fe0ee334fc32d54f0fdf08a88eeb3d33d980
Aaqil Ilyas <mohammedaaqililyas01@gmail.com>
2025-09-10 16:44:29 +0530

    Removed blurWindow Module

--- helpers/__init__.py
+++ helpers/__init__.py
@@ -1,5 +1,4 @@
-from helpers.blurWindow import Blur
 from helpers.center import center_on_screen
 from helpers.styles import Styles
 
-__all__: list[str] = ["Blur", "Styles", "center_on_screen"]
+__all__: list[str] = ["Styles", "center_on_screen"]
